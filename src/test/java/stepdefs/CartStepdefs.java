efa6283c597c622bd10c321a7465961ba1849dfd
Haldun35 <tsmnc054@gmail.com>
2023-12-25 09:52:07 +0100

    DemobellatrixHA

--- src/test/java/stepdefs/CartStepdefs.java
+++ src/test/java/stepdefs/CartStepdefs.java
@@ -4,12 +4,9 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+
 
 import static stepdefs.Locator.*;
 
